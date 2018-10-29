c626d44e03f79fb90372ae48e4efbc4e691f5c11
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-10-29 12:14:37 +0300

    user routes fixed

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,14 @@
+const User = require('../controllers/user');
 
 
 const userRoutes = [
-  { method: 'post', url: '/user/:name/create', func: () => null },
+  { method: 'post', url: '/user/:nickname/create', func: User.create },
   // Создание нового пользователя
-  { method: 'get', url: '/user/:name/profile', func: () => null },
+
+  { method: 'get', url: '/user/:nickname/profile', func: User.get },
   // Получение информации о пользователе
-  { method: 'post', url: '/user/:name/profile', func: () => null },
+
+  { method: 'post', url: '/user/:nickname/profile', func: User.update },
   // Изменение данных о пользователе
 ];
 
