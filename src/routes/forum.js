f834aea8843e59b94b001b29a7c420081067c57b
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-10-29 18:51:45 +0300

    routes improvements

--- src/routes/forum.js
+++ src/routes/forum.js
@@ -1,15 +1,13 @@
+const Forum = require('../controllers/forum');
+
 
 const forumRoutes = [
-  { method: 'post', url: '/forum/create', func: () => null },
-  // Создание форума
-  { method: 'get', url: '/forum/:name/details', func: () => null },
-  // Получение информации о форуме
-  { method: 'post', url: '/forum/:name/create', func: () => null },
-  // Создание ветки
-  { method: 'get', url: '/forum/:name/users', func: () => null },
-  // Пользователи данного форума
-  { method: 'get', url: '/forum/:name/threads', func: () => null },
-  // Список ветвей обсужления форума
+  { method: 'post', url: '/forum/create',         func: Forum.create },
+  { method: 'get',  url: '/forum/:name/details',  func: Forum.details },
+  { method: 'post', url: '/forum/:thread/create', func: Forum.newThread },
+  { method: 'get',  url: '/forum/:name/users',    func: Forum.users },
+  { method: 'get',  url: '/forum/:name/threads',  func: Forum.threads },
 ];
+
 
 module.exports = forumRoutes;
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,14 +2,9 @@ const User = require('../controllers/user');
 
 
 const userRoutes = [
-  { method: 'post', url: '/user/:nickname/create', func: User.create },
-  // Создание нового пользователя
-
-  { method: 'get', url: '/user/:nickname/profile', func: User.get },
-  // Получение информации о пользователе
-
+  { method: 'post', url: '/user/:nickname/create',  func: User.create },
+  { method: 'get',  url: '/user/:nickname/profile', func: User.get },
   { method: 'post', url: '/user/:nickname/profile', func: User.update },
-  // Изменение данных о пользователе
 ];
 
 
