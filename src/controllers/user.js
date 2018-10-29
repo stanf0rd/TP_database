fc960ff510fa9953c2aac60d0f1bf54d18ffcc74
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-10-29 18:45:28 +0300

    User controller: create done, get and update alpha

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -9,13 +9,13 @@ exports.create = async (req, res) => {
     about: req.body.about,
   };
 
-  const conflicted = await User.getConflicted({
-    nickname: userData.nickname,
-    email: userData.email,
-  });
-  if (conflicted.err) throw new Error('Unexpected error.', conflicted.err);
-
-  console.log(conflicted);
+  const conflicted = await User.getConflicted(
+    userData.nickname,
+    userData.email,
+  );
+  if (conflicted.err) {
+    throw new Error('Unable to find conflicts', conflicted.err);
+  }
 
   if (conflicted.users.length !== 0) {
     res.body = conflicted.users;
@@ -24,16 +24,37 @@ exports.create = async (req, res) => {
   }
 
   const created = await User.create(userData);
-  if (created.err) throw new Error('Unexpected error.', created.err);
+  if (created.err) throw new Error('Unable to create user.', created.err);
 
   res.body = created.user;
   res.send(201);
 };
 
-// exports.get = (req, res) => {
-//   console.log(req.params.nickname);
-// };
 
-// exports.update = (req, res) => {
-//   console.log(req.params.nickname);
-// };
+exports.get = async (req, res) => {
+  const { nickname } = req.params;
+  const { err, users } = await User.get('nickname', nickname);
+  if (err) throw new Error('Unable to get user', err);
+
+  // TODO: FIXME: 404 if can't find user
+
+  [res.body] = users;
+  res.send(200);
+};
+
+
+exports.update = async (req, res) => {
+  const userData = {
+    nickname: req.params.nickname,
+    fullname: req.body.fullname,
+    email: req.body.email,
+    about: req.body.about,
+  };
+
+  // TODO: FIXME: many other cases
+
+  const { err, result } = await User.update(userData);
+
+  res.body = userData;
+  res.send(200);
+};
