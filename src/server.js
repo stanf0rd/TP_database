c5e2cb6beda5cf708ffbdb0dd6958f1efa91d370
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-10-29 12:13:11 +0300

    simple server cleared

--- src/server.js
+++ src/server.js
@@ -4,17 +4,16 @@ const morgan  = require('morgan');
 
 const routes = require('./routes');
 
+
 const service = restana();
 service.use(parser.json());
 service.use(morgan('dev'));
 
 
-console.log(`Worker ${process.pid} started...`);
-
-
 routes.forEach((route) => {
   const { method, url, func } = route;
-  service[method](url, func);
+  service[method](`/api${url}`, func);
 });
+
 
 service.start(5000);
