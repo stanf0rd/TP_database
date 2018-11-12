febdba67b8ed43f8c0280fceb7f0198124d565f8
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-12 05:31:47 +0300

    threads table to schema.sql

--- src/utils/schema.sql
+++ src/utils/schema.sql
@@ -3,33 +3,43 @@ https://stackoverflow.com/questions/15981197/postgresql-error-type-citext-does-n
 */
 
 -- drop all
+DROP TABLE IF EXISTS threads;
 DROP TABLE IF EXISTS forums;
-DROP TABLE IF EXISTS forum_users;
+DROP TABLE IF EXISTS "users";
 
 
 -- users
-CREATE TABLE forum_users (
-  nickname  citext   NOT NULL PRIMARY KEY,
-  email     citext   NOT NULL,
-  fullname  varchar  NOT NULL,
-  about     varchar  NOT NULL
+CREATE TABLE "users" (
+  nickname   citext     NOT NULL    PRIMARY KEY,
+  email      citext     NOT NULL,
+  fullname   text       NOT NULL,
+  about      text       NOT NULL
 );
 
--- DROP INDEX IF EXISTS index_on_users_nickname;
 DROP INDEX IF EXISTS index_on_users_email;
 
--- CREATE UNIQUE INDEX index_on_users_nickname
-  -- ON forum_users (lower(nickname));
-
 CREATE UNIQUE INDEX index_on_users_email
-  ON forum_users (email);
+  ON "users" (email);
 
 
 -- forums
 CREATE TABLE forums (
-  title        citext    NOT NULL,
-  slug         citext    NOT NULL   PRIMARY KEY,
-  "user"       citext    NOT NULL   REFERENCES forum_users (nickname),
-  posts        integer   NOT NULL   DEFAULT 0,
-  threads      integer   NOT NULL   DEFAULT 0
+  title      citext     NOT NULL,
+  slug       citext     NOT NULL    PRIMARY KEY,
+  "user"     citext     NOT NULL    REFERENCES "users" (nickname),
+  posts      integer    NOT NULL    DEFAULT 0,
+  threads    integer    NOT NULL    DEFAULT 0
+);
+
+
+-- threads
+CREATE TABLE threads (
+  id         serial     NOT NULL    PRIMARY KEY,
+  title      text       NOT NULL,
+  author     citext     NOT NULL    REFERENCES "users" (nickname),
+  forum      citext     NOT NULL    REFERENCES forums (slug),
+  message    text       NOT NULL,
+  votes      integer                DEFAULT 0,
+  slug       text                   DEFAULT NULL,
+  created  timestamptz  NOT NULL    DEFAULT CURRENT_TIMESTAMP
 );
