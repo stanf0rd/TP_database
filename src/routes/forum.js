b3f872db449834aae973c7ece0b7a5a6ece0be2a
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-12 05:32:02 +0300

    cleaning up

--- src/routes/forum.js
+++ src/routes/forum.js
@@ -4,8 +4,8 @@ const Forum = require('../controllers/forum');
 const forumRoutes = [
   { method: 'post', url: '/forum/create',         func: Forum.create },
   { method: 'get',  url: '/forum/:slug/details',  func: Forum.details },
-  { method: 'get',  url: '/forum/:slug/users',    func: Forum.users },
   { method: 'get',  url: '/forum/:slug/threads',  func: Forum.threads },
+  { method: 'get',  url: '/forum/:slug/users',    func: Forum.users },
 ];
 
 
