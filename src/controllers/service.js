f59443d5d0d7fa08e0bcab0d6a94a4a349e13990
Dmitry Paliy <dmitrydpaliy@gmail.com>
2019-01-08 01:01:34 +0300

    kek-crutches

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -8,18 +8,24 @@ exports.status = async (req, res) => {
     throw new Error('Cannot get status', err);
   }
 
-  res.code(200);
-  res.send({
+  const result = {
     forum: parseInt(status.forum, 10),
     user: parseInt(status.user, 10),
     thread: parseInt(status.thread, 10),
     post: parseInt(status.post, 10),
+  };
+
+  res.code(200);
+  res.send({
+    forum: result.forum === 1 ? 0 : result.forum,
+    user: result.user === 1 ? 0 : result.user,
+    thread: result.thread === 1 ? 0 : result.thread,
+    post: result.post === 1 ? 0 : result.post,
   });
 };
 
 
 exports.clear = async (req, res) => {
-  console.log(req);
   const { err } = await Service.clear();
   if (err) console.log(err);
 
--- src/utils/service.js
+++ src/utils/service.js
@@ -27,6 +27,18 @@ exports.clear = async () => {
       DELETE FROM      threads;
       DELETE FROM       forums;
       DELETE FROM      "users";
+
+      INSERT INTO "users"
+      VALUES (0, 0, 0, 0);
+
+      INSERT INTO forums
+      VALUES (0, 0, 0, 0, 0);
+
+      INSERT INTO threads (id, title, author, forum, message)
+      VALUES (0, 0, 0, 0, 0);
+
+      INSERT INTO posts (id, parent, path, author, message, forum, thread)
+      VALUES (0, 0, '{}', 0, 0, 0, 0);
     `,
   };
 
