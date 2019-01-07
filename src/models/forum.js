862a7b73576ebee53e592f53e95c5db05530ff58
Dmitry Paliy <dmitrydpaliy@gmail.com>
2019-01-07 03:52:04 +0300

    get and sort useres

--- src/models/forum.js
+++ src/models/forum.js
@@ -4,6 +4,7 @@ const db = require('../utils/database');
 class Forum {
   constructor() {
     this.table = 'forums';
+    this.userTable = 'user_posts';
     this.rows = ['title', 'slug', '"user"'];
   }
 
@@ -17,7 +18,7 @@ class Forum {
         VALUES(
           $1,
           $2,
-          (SELECT nickname FROM "users" WHERE nickname=$3)
+          (SELECT nickname FROM "users" WHERE nickname=$3 LIMIT 1)
         )
         RETURNING *;
       `,
@@ -44,6 +45,40 @@ class Forum {
     if (err) return { err };
 
     return { forums: result.rows };
+  }
+
+
+  async getUsers(slug, options) {
+    const parsedOptions = options || {};
+    const limit = 'limit' in parsedOptions ? parsedOptions.limit : null;
+    const desc = 'desc' in parsedOptions ? parsedOptions.desc : null;
+    const since = 'since' in parsedOptions ? parsedOptions.since : null;
+
+    let sinceExpr = '';
+    if (since) {
+      sinceExpr = desc === 'true'
+        ? `AND nickname < '${since}' COLLATE "C"`
+        : `AND nickname > '${since}' COLLATE "C"`;
+    }
+
+    const query = {
+      text: `
+        SELECT * FROM "users"
+        WHERE nickname IN (
+          SELECT "user" FROM ${this.userTable}
+          WHERE forum = '${slug}'
+        )
+        ${sinceExpr}
+        ORDER BY nickname COLLATE "C" ${desc === 'true' ? 'DESC' : 'ASC'}
+        ${limit ? `LIMIT ${limit}` : ''}
+      `,
+    };
+
+    console.log(query.text);
+
+    const { err, result } = await db.makeQuery(query);
+    if (err) return { err };
+    return { users: result.rows };
   }
 }
 
